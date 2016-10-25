7b3bf3ac5354e9bacb4c6bdb8fe683ac97207710
Carlos <Carlos@ccarfer>
2016-10-25 13:19:13 +0100

    Creacion de la clase intento y desarrollo del metodo generar intervalo junto cojugarAdivinaNumero

---
+++ src/modelo/Intento.java
@@ -0,0 +1,25 @@
+package modelo;
+
+import java.util.Date;
+
+public class Intento {
+	private int numero;
+	private Date fechaintento;
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Date getFechaintento() {
+		return fechaintento;
+	}
+
+	public void setFechaintento(Date fechaintento) {
+		this.fechaintento = fechaintento;
+	}
+
+}

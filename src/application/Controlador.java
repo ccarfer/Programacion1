dfc66eae6fe92cc8432861632829e54e2af3127a
Carlos <Carlos@ccarfer>
2016-11-07 08:41:12 +0000

    Añadimos programa JavaFX Scene Builder 2.0 con el paquete aplicacion

---
+++ src/application/Controlador.java
@@ -0,0 +1,12 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class Controlador {
+	public void manejaBoton(ActionEvent evento) {
+		System.out.println("Pulsando");
+	}
+	public void manejaBoton2(ActionEvent evento) {
+		System.out.println("2º Boton pulsado");
+	}
+}

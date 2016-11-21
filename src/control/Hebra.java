ad06da672defbb8b091cbf08c4e297c6493e767d
Carlos <Carlos@ccarfer>
2016-11-21 10:09:36 +0000

    actualizacion 21/11/2016

--- src/control/Hebra.java
+++
@@ -1,8 +0,0 @@
-package control;
-
-public class Hebra extends Thread {
-	public void run() {
-		for (int i = 0; i < Integer.MAX_VALUE; i++)
-			System.out.println(Thread.currentThread().getName() + " : " + i);
-	}
-}
